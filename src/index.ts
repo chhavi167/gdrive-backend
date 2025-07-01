9f18428934577ed3e65816c9dac2b3378e5557b3
chhavi167 <chhavirathi246@gmail.com>
2025-07-01 10:41:18 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "https://gdrive-frontend-dc57.vercel.app",
-  "https://gdrive-frontend-dc57-n330p30k6-chhavis-projects-3aa15ac8.vercel.app",
-  "https://gdrive-frontend-dc57-git-main-chhavis-projects-3aa15ac8.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
