2a4efae79d92cd6790b99cd9f16b7581dbc53bfd
chhavi167 <chhavirathi246@gmail.com>
2025-07-01 10:12:22 +0530

    origin issue solved

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
-  "http://localhost:5173", // for local dev
-  "https://gdrive-frontend-dc57.vercel.app", // your deployed frontend
+  "http://localhost:5173", 
+  "https://gdrive-frontend-dc57.vercel.app", 
 ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
